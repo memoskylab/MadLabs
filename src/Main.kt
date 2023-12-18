826e7b8d04e48553471e821f3d224ee76dd99165
Mohammad Aslam <memoskylab@gmail.com>
2023-12-18 13:36:54 +0800

    This is another project of mine.

--- src/Main.kt
+++ src/Main.kt
@@ -32,8 +32,8 @@ fun main () {
     val work = readLine()
 
     print("""
-    A long time ago, $name and their friend $friendsName planned a(n) $adjective road trip to see their favorite band: $bandName. 
-    The venue was $distance miles away which meant they needed to save $$totalForGas dollars to get there.\n
+    A long time ago, $name and their friend $friendsName planned a $adjective road trip to see their favorite band: $bandName. 
+    The venue was $distance miles away which meant they needed to save $totalForGas dollars to get there.
     Both of them worked as $work for the whole summer and managed to save up a total of $totalMoneySaved dollars!
     To celebrate, they ended up driving to $city for the whole weekend; seeing their band and buying $bandName t-shirts to remember their trip.
     """.trimIndent())
